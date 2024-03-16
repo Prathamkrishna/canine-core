57a94640fc98582e9d30480f3c8685571959b983
Pratham Krishna <prathamkrishna@Prathams-MacBook-Air.local>
2024-03-17 00:12:59 +0530

    Pre-release test for arm and amd

--- cfssl/cfssl-installer.sh
+++ cfssl/cfssl-installer.sh
@@ -1,5 +1,12 @@
-wget -q --show-progress --https-only --timestamping https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssl_1.6.5_linux_arm64 https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssljson_1.6.5_linux_arm64
-chmod +x cfssl_1.6.5_linux_arm64 cfssljson_1.6.5_linux_arm64
-sudo mv cfssl_1.6.5_linux_arm64 /usr/local/bin/cfssl
-sudo mv cfssljson_1.6.5_linux_arm64 /usr/local/bin/cfssljson
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    ARCH="amd64"
+elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
+    ARCH="arm64"
+fi
+
+wget -q --show-progress --https-only --timestamping https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssl_1.6.5_linux_${ARCH} https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssljson_1.6.5_linux_${ARCH}
+chmod +x cfssl_1.6.5_linux_${ARCH} cfssljson_1.6.5_linux_${ARCH}
+sudo mv cfssl_1.6.5_linux_${ARCH} /usr/local/bin/cfssl
+sudo mv cfssljson_1.6.5_linux_${ARCH} /usr/local/bin/cfssljson
 
